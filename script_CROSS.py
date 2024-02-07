771ab3855a0f123439718a4abb09370661dfd008
François Niagoloff <francois.niagoloff@e-csi.fr>
2024-02-07 18:13:35 +0100

    Finished Script_cross

--- script_CROSS.py
+++ script_CROSS.py
@@ -4,12 +4,10 @@ import pandas as pd
 chemin_sources = '/home/laptopus/Bureau/SCRIPT_MARGE_BRUT/DATA/sources.xlsx'
 chemin_marge_brute = '/home/laptopus/Bureau/SCRIPT_MARGE_BRUT/SORTIE/marge_brute.xlsx'
 
-
-# Fonction pour nettoyer les noms de colonnes (retirer les espaces en trop)
+# Fonction pour nettoyer les noms de colonnes
 def nettoyer_noms_colonnes(df):
     df.columns = df.columns.str.strip()
     return df
-
 
 # Lire les noms des feuilles de calcul et trouver celle qui contient "BDD"
 nom_feuille = None
@@ -29,14 +27,15 @@ if nom_feuille:
     sources = nettoyer_noms_colonnes(sources)
     marge_brute = nettoyer_noms_colonnes(marge_brute)
 
+    # Supprimer la colonne "Commentaire" si elle existe
+    if "Commentaire" in marge_brute.columns:
+        marge_brute.drop(columns=["Commentaire"], inplace=True)
+
     # Fusion des données basée sur la correspondance des clés
-    resultat = pd.merge(marge_brute, sources, left_on='Section Analytique - Code', right_on='codes analytiques',
-                        how='left')
+    resultat = pd.merge(marge_brute, sources, left_on='Section Analytique - Code', right_on='codes analytiques', how='left')
 
     # Sélection des colonnes à copier
-    colonnes_a_copier = ["TYPE", "BL", "Groupe", "Compte", "GC", "Intérêt", "Session", "Date de fin", "Type de groupe",
-                         "Secteur d'activité", "Titre du produit", "BL d'origine", "grand dispo ou pas",
-                         "mode Formation", "regroupements Secteurs d'Activité", "pays facturation"]
+    colonnes_a_copier = ["TYPE", "BL", "Groupe", "Compte", "GC", "Intérêt", "Session", "Date de fin", "Type de groupe", "Secteur d'activité", "Titre du produit", "BL d'origine", "grand dispo ou pas", "mode Formation", "regroupements Secteurs d'Activité", "pays facturation"]
 
     # Copier les colonnes spécifiques
     for colonne in colonnes_a_copier:
