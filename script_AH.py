b6491923db7a966557c8d20c0872c3dd729b15c7
François Niagoloff <francois.niagoloff@e-csi.fr>
2024-02-07 11:54:32 +0100

    Merged the scripts

--- script_AH.py
+++ script_AH.py
@@ -33,7 +33,46 @@ def categoriser(ligne):
         return bl  # Retourne la valeur existante si aucune règle ne s'applique
 
 
-# Application des règles directement à la colonne 'BL'
+# Fonction pour appliquer les nouvelles règles
+def categoriser_nouvelle_bp(ligne):
+    mode_formation_bp = ligne['Mode de Formation Version BP'].strip()
+    mode_formation = ligne['mode de formation'].strip()
+    titre_produit = ligne['Titre du produit'].lower()  # Convertir en minuscules pour la recherche
+    bl = ligne['BL'].strip()
+
+    # Appliquer les nouvelles règles
+    if mode_formation_bp == "Réglementaire" and mode_formation in ["Corporate MOOC", "Executive MOOC"]:
+        return "DL REGLEMENTAIRE"
+    elif mode_formation_bp == "Réglementaire" and mode_formation == "In house training":
+        return "INTRA PRÉSENTIEL RÉGLEMENTAIRE"
+    elif mode_formation_bp == "Regroupement Réglementaire" and mode_formation == "Public training":
+        return "INTER PRÉSENTIEL RÉGLEMENTAIRE"
+    elif bl != "Regroupement Réglementaire" and mode_formation == "Public training":
+        return "INTER PRÉSENTIEL HORS REGLEMENTAIRE"
+    elif bl != "Regroupement Réglementaire" and mode_formation == "In house training":
+        return "INTRA PRÉSENTIEL HORS REGLEMENTAIRE"
+    elif mode_formation_bp == "EOC" and any(x in titre_produit for x in ["hec", "cbs", "financ"]):
+        return "FEO FINANCE"
+    elif mode_formation_bp == "EOC" and not any(x in titre_produit for x in ["hec", "cbs", "financ"]):
+        return "FEO NON FINANCE"
+    else:
+        return "Autre"
+
+
+# Application de la première règle directement à la colonne 'BL'
 df['Mode de Formation Version BP'] = df.apply(categoriser, axis=1)
+
+# Application de la deuxième règle pour créer la nouvelle colonne temporairement
+df['Mode de formation new BP Temp'] = df.apply(categoriser_nouvelle_bp, axis=1)
+
+# Trouver l'index de la colonne 'Mode de Formation Version BP'
+index_mode_formation_version_bp = df.columns.get_loc('Mode de Formation Version BP')
+
+# Insérer la colonne 'Mode de formation new BP' à l'index désiré
+df.insert(index_mode_formation_version_bp + 1, 'Mode de formation new BP', df['Mode de formation new BP Temp'])
+
+# Supprimer la colonne temporaire
+df.drop('Mode de formation new BP Temp', axis=1, inplace=True)
+
 # Sauvegarde dans un nouveau fichier Excel
 df.to_excel(chemin_sortie, index=False)
